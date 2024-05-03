6b07d06914556f71e8a235b851ae9d9debec06f5
fb24m <fakem1ner@yandex.ru>
2024-05-04 00:39:44 +0400

    fix custom page error

--- src/app/[locale]/p/[slug]/page.tsx
+++ src/app/[locale]/p/[slug]/page.tsx
@@ -1,5 +1,7 @@
-const CustomPage = ({ slug }: { slug: string }) => {
+const CustomPage = ({ params }: { params: { slug: string } }) => {
 	return (
-
+		<>Custom page with slug {params.slug} will be here</>
 	)
 }
+
+export default CustomPage
