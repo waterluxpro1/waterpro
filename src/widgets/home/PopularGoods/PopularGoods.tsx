54859d335eac6590965f3a534d02e4c51cdf31f7
fb24m <fakem1ner@yandex.ru>
2024-05-19 16:56:04 +0400

    hpe don't needed fixing. Turn all back

--- src/widgets/home/PopularGoods/PopularGoods.tsx
+++ src/widgets/home/PopularGoods/PopularGoods.tsx
@@ -1,7 +1,7 @@
 import { Container } from '@/shared/ui/Container'
 import styles from './PopularGoods.module.scss'
 import { Title2 } from '@/shared/ui/Title2'
-// import { GoodsSlider } from '@/features/GoodsSlider/GoodsSlider'
+import { GoodsSlider } from '@/features/GoodsSlider/GoodsSlider'
 import { woocomerence } from '@/shared/api/wordpress.service'
 import type { PopularGoodsModel } from './PopularGoods.model'
 
@@ -12,7 +12,7 @@ export const PopularGoods = async ({ lang = 'ru', locale }: PopularGoodsModel) =
 		<Container>
 			<Title2 className={styles.title}>{locale?.title}</Title2>
 			{JSON.stringify(goods)}
-			{/* <GoodsSlider lang={lang} className={styles.slider} goods={goods} /> */}
+			<GoodsSlider lang={lang} className={styles.slider} goods={goods} />
 		</Container>
 	)
 }
