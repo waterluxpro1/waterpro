9509b16dd3ea2f3e4956a042be9f641da188b4f3
fb24m <fakem1ner@yandex.ru>
2024-05-19 16:43:18 +0400

    Return advantages widget (hpe, t2)

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { Welcome } from '@/widgets/home/Welcome'
 import { Categories } from '@/widgets/home/Categories'
-// import { Advantages } from '@/widgets/home/Advantages'
+import { Advantages } from '@/widgets/home/Advantages'
 // import { Contact } from '@/widgets/home/Contact/Contact'
 // import { Faq } from '@/widgets/home/Faq/Faq'
 // import { PopularGoods } from '@/widgets/home/PopularGoods/PopularGoods'
@@ -21,11 +21,11 @@ export default async function Home({ params }: { params: { locale: string } }) {
       <div className={styles.page}>
         <Welcome locale={lang.welcome} />
         <Categories {...lang.categories} />
-        {/* <Advantages locale={lang.advantages} />
-        <PopularGoods locale={lang.popular_goods} lang={params.locale} />
-        <Contact />
-        <Reviews locale={lang.reviews} />
-        <Faq locale={lang.faq} /> */}
+        <Advantages locale={lang.advantages} />
+        {/* <PopularGoods locale={lang.popular_goods} lang={params.locale} /> */}
+        {/* <Contact /> */}
+        {/* <Reviews locale={lang.reviews} /> */}
+        {/* <Faq locale={lang.faq} /> */}
       </div>
     )
   }
