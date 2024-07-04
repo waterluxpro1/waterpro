ff026efc689fc0e6b99cf70833069b1152bd88b7
fb24m <fakem1ner@yandex.ru>
2024-07-04 16:36:46 +0400

    fix translations loading in ContactForm & ReviesSlider components

--- src/features/ContactForm/ContactForm.tsx
+++ src/features/ContactForm/ContactForm.tsx
@@ -3,9 +3,7 @@ import { headers } from 'next/headers'
 import { ContactFormTemplate } from './ContactFormTemplate'
 
 export const ContactForm = async () => {
-	const translations = await wordpress.getTranslations('contact-form', headers().get('referer')?.split('/')[3]!)
-
-	return (
+	return wordpress.getTranslations('contact-form', headers().get('referer')?.split('/')[3]!).then((translations) =>
 		<ContactFormTemplate translations={translations} />
 	)
 }
--- src/features/ReviewsSlider/ReviewsSlider.tsx
+++ src/features/ReviewsSlider/ReviewsSlider.tsx
@@ -4,9 +4,7 @@ import { ReviewsSliderTemplate } from './ReviewsSliderTemplate'
 import { headers } from 'next/headers'
 
 export const ReviewsSlider = async ({ className }: { className?: string }) => {
-	const translations = await wordpress.getTranslations('reviews-slider', headers().get('referer')?.split('/')[3]!)
-
-	return (
+	return wordpress.getTranslations('reviews-slider', headers().get('referer')?.split('/')[3]!).then((translations) =>
 		<ReviewsSliderTemplate className={className} translations={translations} />
 	)
 }
